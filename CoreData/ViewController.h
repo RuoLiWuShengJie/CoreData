79c336441246bac3f4d4e2d32f3cb13a425b5fa0
RuoLiWuShengJie <469912605@qq.com@wushengjie.local>
2016-04-16 11:47:07 +0800

    Initial Commit

---
+++ CoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreData
+//
+//  Created by 吴胜杰 on 16/4/16.
+//  Copyright © 2016年 wsj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreData/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CoreData
+//
+//  Created by 吴胜杰 on 16/4/16.
+//  Copyright © 2016年 wsj. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
